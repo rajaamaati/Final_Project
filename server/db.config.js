f88a74ce8643d67a56b445336aea3f708fc1fea5
rajaamaati <rajaamaati1998@gmail.com>
2023-05-04 14:00:58 +0200

    first version1

---
+++ server/db.config.js
@@ -0,0 +1,7 @@
+const dbName = 'food-ordering';
+const dbHost = 'localhost';
+const dbPort = 27017;
+module.exports = {
+    url: `mongodb://${dbHost}:${dbPort}/${dbName}`
+}
+//url = mongodb+srv://rajaamaati1998:rajaamaati1998@cluster0.jxtomnf.mongodb.net/test
